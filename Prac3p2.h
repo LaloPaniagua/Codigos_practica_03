1196e392ea9630347f7b76ff38a50aa16e254648
LaloPaniagua <65317268+LaloPaniagua@users.noreply.github.com>
2021-04-02 18:42:12 -0600

    Update Prac3p2.h
    Esta versión ya funciona perfectamente, cualquier duda preguntenme

--- Prac3p2.h
+++ Prac3p2.h
@@ -1,16 +1,25 @@
+#include <stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
-#ifdef COMPLEJOS_H
-#define COMPLEJOS_H
+#ifndef PRAC3P2_H
+#define PRAC3P2_H
 
+//Estructura del término del polinomio que consiste de un coeficiente y un exponente
+struct _termino{
+    int exponente;
+    double coeficiente;
+};
+typedef struct _termino TERMINO;
+//Estructura del polinomio compuesta por un campo con la cantidad de términos del polinomio y un apuntador que contendrá a loos términos
 struct _polinomio{
-    double *p;
-    int grado;
+    int terminos;
+    TERMINO* P;
 };
 typedef struct _polinomio POLI;
-POLI* crearpoli();
-void resta(POLI *a, POLI *b);
-void suma(POLI *a, POLI *b);
-void imprimir(POLI *a)
-void liberar(POLI *a);
+//Prototipos de las funciones
+POLI crearpolinomio();
+POLI* resta(POLI a, POLI b);
+POLI* suma(POLI a, POLI b);
+void imprimir(POLI a);
+void liberar(POLI a);
 #endif
