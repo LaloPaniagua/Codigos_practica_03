689c2372208234256352e1997a5a70b4ef20991c
LaloPaniagua <65317268+LaloPaniagua@users.noreply.github.com>
2021-04-02 18:41:38 -0600

    Update Prac3p2.c
    Esta versión ya funciona perfectamente, cualquier duda preguntenme

--- Prac3p2.c
+++ Prac3p2.c
@@ -1,114 +1,129 @@
-#include<stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "Prac3p2.h"
-/* NOTAS GENERALES PA LOS PANAS
-    Actualmente no puedo correr el programa porque me da error con todo el rollo de POLI y los asteriscos usados para los apuntadores. 
-    Trtaen de correrlo en sus compus y me dicen que les sale.
-    No se porque no esta identificando el struc del .h
-*/
-
-
-/*
-Para crear el poli se debe saber primero el grado, el cual se almacena dentro del struc como un int
-De acuerdo al grado se va almacenando memoria para eso.
-El ciclo for es para almacenar los datos de cada temino.
-*/
-POLI* crearpoli(){
-        POLI nuevo;
-        printf("Cual es el grado del polinomio?\n");
-        scanf("%d",&nuevo->grado);
-        nuevo = (POLI*)malloc(nuevo->grado*sizeof(POLI));
-        printf("Ingrese los valores de su polinomio, empezando por el termino independiente\n");
-        for(int i = 0; i <= nuevo->grado; i++){
-        printf("Valor del coeficiente grado %i: ", i);
-        scanf("%f", &nuevo->p[i]);
-        }
-        imprimir(POLI *nuevo);//Puede haver problemas con la invocacion de esta funcion
-        return *nuevo;
-}
-/*
-El if sirve para primero identificar si el grado del polinomio 1 es mayor al del 2 para usarlo como la base para la suma.
-si a no es mayor, entonces b es mayor. En caso de que b=a o a=b, no importa cual de los polinomiosse use como base.
-Por eso solo hay un if y else.
-Lo mismo pasa con la resta pero negativo
-*/
-void suma(POLI *a, POLI *b){
-    if (a->grado > b->grado){  
-    for(int i=0;i<=b->grado;i++)
-        a->p[i] = a->p[i] + b->p[i];
-    imprimir(POLI *a);
-    }else{
-     for(int i=0;i<=a->grado;i++)
-        b->p[i] = b->p[i] + a->p[i];
-    imprimir(POLI *b);
-    }
-}
-void resta(POLI *a, POLI *b){
-if (a->grado > b->grado){  
-    for(int i=0;i<=b->grado;i++)
-        a->p[i] = a->p[i] - b->p[i];
-    imprimir(POLI *a);
-    }else{
-     for(int i=0;i<=a->grado;i++)
-        b->p[i] = b->p[i] - a->p[i];
-    imprimir(POLI *b);
-    }
-}
-/*
-Para imprimir empezamos por el grado mas alto y vamos al mas pequeño
-El if else if sirve para que se imprima de esta forma: "9x^4+9x^3+6x^2+5x+6"
-*/
-void imprimir(*a){
-    printf("Su polinomio es: ");
-    for(int i = a->grado; i >= 0; i--)
+#include "prac3p2.h"
+//Función para crear un polinomio
+POLI crearpolinomio(){
+    int i=0;
+    POLI p;//Variable de tipo POLI que será retornada tras ejecutarse la función.
+    p.P=NULL;
+    printf("Ingrese el número de términos que tendrá el polinomio\n");
+    scanf("%d",&p.terminos);//El usuario ingresa el número de términos
+    while (p.terminos<=1)//Se asegura que haya por lo menos 2 términos
     {
-        if(i == 0)
-        {
-            printf("%0.2lf",  a->p[i]);
+        puts("El polinomio no puede tener menos de dos términos, ingrese un valor distinto");
+        scanf("%d",&p.terminos);
+    }
+    
+    p.P=malloc(sizeof(TERMINO)*p.terminos);//Se reserva la memoria necesaria para almacenar un polinomio del tamaño especificado
+    for(i;i<p.terminos;i++){//Se asignan los coeficientes de cada término
+        printf("Ingrese el valor del coeficiente correspondiente al término de grado %d :",i);
+        scanf("%lf",&p.P[i].coeficiente);
+        p.P[i].exponente=i;
+    }
+    return p;
+}
+
+POLI* resta(POLI a, POLI b){
+    int i=0;
+    POLI* respuesta;//Apuntador que será retornado tras ejecutarse la función
+    respuesta=malloc(sizeof(POLI));//Se reserva la memoria necesaria para almacenar al polinomio resultante
+    if(a.terminos>=b.terminos)
+        respuesta->terminos=a.terminos;
+    else
+        respuesta->terminos=b.terminos;//Al polinomio respuesta se le asigna el tamaño del polinomio más grande
+    respuesta->P=malloc(sizeof(TERMINO)*respuesta->terminos);//Se reserva la memoria necesaria para almacenar loos términos del polinomio resultante
+    while(i<a.terminos && i<b.terminos){//Se realiza la resta de polinomios
+        respuesta->P[i].coeficiente=a.P[i].coeficiente-b.P[i].coeficiente;
+        respuesta->P[i].exponente=i;
+        i++;
+    }
+    if(respuesta->terminos>=i){//Se llenan los términos restantes con los coeficientes del polinomio de mayor grado
+        if(a.terminos>b.terminos){
+            for(i;i<respuesta->terminos;i++){
+                respuesta->P[i].coeficiente=a.P[i].coeficiente;
+                respuesta->P[i].exponente=a.P[i].exponente;
+            }
         }
-        else if(i == 1)
-        {
-            printf("%0.2lfx + ",  a->p[i]);
-        }
-        else
-        {
-            printf("%0.2lfx^%i + ", a->p[i], i);
+        else{
+            for(i;i<respuesta->terminos;i++){
+                respuesta->P[i].coeficiente=b.P[i].coeficiente;
+                respuesta->P[i].exponente=b.P[i].exponente * -1;
+            }
         }
     }
+    return respuesta;
 }
+
+//La función suma es igual a la función resta, solo cambiando la operación realizada entre los coeficientes
+POLI* suma(POLI a, POLI b){
+    int i=0;
+    POLI* respuesta;
+    respuesta=malloc(sizeof(POLI));
+    if(a.terminos>=b.terminos)
+        respuesta->terminos=a.terminos;
+    else
+        respuesta->terminos=b.terminos;
+    respuesta->P=malloc(sizeof(TERMINO)*respuesta->terminos);
+    while(i<a.terminos && i<b.terminos){
+        respuesta->P[i].coeficiente=a.P[i].coeficiente+b.P[i].coeficiente;
+        respuesta->P[i].exponente=i;
+        i++;
+    }
+   if(respuesta->terminos>=i){
+        if(a.terminos>b.terminos){
+            for(i;i<respuesta->terminos;i++){
+                respuesta->P[i].coeficiente=a.P[i].coeficiente;
+                respuesta->P[i].exponente=a.P[i].exponente;
+            }
+        }
+        else{
+            for(i;i<respuesta->terminos;i++){
+                respuesta->P[i].coeficiente=b.P[i].coeficiente;
+                respuesta->P[i].exponente=b.P[i].exponente;
+            }
+        }
+    }
+    return respuesta;
+}
+//Función para imprimir los polinomios
+void imprimir(POLI a){
+    int i=a.terminos-1;//Variable con la posición de cada elemento del arreglo
+    for(i;i>1;i--){//Ciclo para imprimir cada elemento del arreglo de grado superior a 2
+        printf("%lf x^%d ",a.P[i].coeficiente,a.P[i].exponente);
+        if(a.P[i-1].coeficiente>=0)
+            printf("+ ");
+    }
+    printf("%lf x ",a.P[i].coeficiente);//Impresión del término lineal
+        if(a.P[0].coeficiente>=0)
+            printf("+ ");
+    printf("%lf\n",a.P[0].coeficiente);//Impresión del término independiente
+}
+
+void liberar(POLI a){
+    free(a.P);
+    a.terminos=0;
+}
+
 int main(){
-    int x;
-    //Se declaran los 2 polis a crear
-    POLI *a,*b;
-    *a=crearpoli();
-    *b=crearpoli();
-    printf("Que operacion desea realizar?\n 1-suma \n 2-resta");
-    scanf("&d",&x);
-    //La neta no me acordaba como se usa el switch, espero que esto sea lo correcto.
-    switch (x)
-    {
-    case 1:
-        suma(POLI *a, POLI *b);
-        break;
-    case 2:
-        resta(POLI *a, POLI *b);
-        break;
-    default:
-        printf("\nOpcion no valida");
-        break;
-    return 0xF;
+	int i=0,o=0;
+    puts("Primer polinomio:");
+    POLI p1=crearpolinomio();
+    puts("Segundo polinomio:");
+    POLI p2=crearpolinomio();
+    POLI* r=NULL;
+    printf("Seleccione la operación a realizar\n1)Suma\n2)Resta\n");
+    scanf("%d",&o);
+    while(o<1||o>2){
+        puts("No se ha seleccionado una operación válida");
+        scanf("%d",&o);
     }
-    
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    if(o==1)
+        r=suma(p1,p2);
+    else
+        r=resta(p1,p2);
+    puts("Resultado:");
+    imprimir(*r);
+    liberar(p1);
+    liberar(p2);
+    liberar(*r);
+    free(r);
+    return 1;
 }
